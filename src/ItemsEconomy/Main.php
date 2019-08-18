2063b687897373b37e5350eca9dc7805ae79b97a
PawarenessC <35727789+PawarenessC@users.noreply.github.com>
2019-08-18 09:58:42 +0900

    とりあえず今はこんな感じ

--- src/ItemsEconomy/Main.php
+++ src/ItemsEconomy/Main.php
@@ -9,4 +9,27 @@ use pocketmine\utils\Config;
 use pocketmine\Server;
 use pocketmine\Player;
 
-class Main extends pluginBase implements Listener{}
+class Main extends pluginBase implements Listener{
+	
+	public function onEnable(){
+		$this->getLogger()->info("=========================");
+ 		$this->getLogger()->info("ItemsEconomyを読み込みました");
+ 		$this->getLogger()->info("制作者: PawarenessC");
+ 		$this->getLogger()->info("バージョン:{$this->getDescription()->getVersion()}");
+ 		$this->getLogger()->info("=========================");
+		
+		$this->configInit();
+		$this->registerEvents();
+		$this->Init();
+	}
+	
+	public function configInit(){
+		$this->data = new Config($this->getDataFolder() . "Datas.yml", Config::YAML);
+	}
+	
+	public function registerEvents(){
+	}
+	
+	public function Init(){
+		$api = new APIs($this);
+	}
