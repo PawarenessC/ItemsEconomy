7e0592bcac931b652f4da347dfa38be283e679e4
PawarenessC <35727789+PawarenessC@users.noreply.github.com>
2019-08-18 09:47:55 +0900

    Create Main.php

---
+++ src/ItemsEconomy/Main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ItemsEconomy;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+
+use pocketmine\Server;
+use pocketmine\Player;
+
+class Main extends pluginBase implements Listener{}
